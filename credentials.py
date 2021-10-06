d2e2c73538948447074fcbac97e69c274c5695a8
Gaming-Rowdies <59770086+Gaming-Rowdies@users.noreply.github.com>
2021-10-07 01:29:20 +0530

    Created `spotify` client

---
+++ credentials.py
@@ -0,0 +1,15 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
+
+# Scope for required features
+scope = "ugc-image-upload user-read-recently-played user-top-read user-read-playback-position user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-follow-modify user-follow-read user-library-modify user-library-read user-read-email user-read-private"
+
+client_data = {"client_id": "YOUR_CLIENT_ID",
+               "client_secret": "YOUR_CLIENT_SECRET"}
+
+# Spotify's login details
+creds = SpotifyClientCredentials(**client_data)
+auth = SpotifyOAuth({**client_data, "scope": scope,
+                    "redirect_uri": "http://localhost:8080"})
+
+spotify = spotipy.Spotify(auth_manager=auth)
