a14383619eb36db3314fc927b93d3545287af07b
Gaming-Rowdies <59770086+Gaming-Rowdies@users.noreply.github.com>
2021-10-08 14:51:01 +0530

    Relocated credentials file

--- main/credentials.py
+++ main/credentials.py
@@ -4,12 +4,12 @@ from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 # Scope for required features
 scope = "ugc-image-upload user-read-recently-played user-top-read user-read-playback-position user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-follow-modify user-follow-read user-library-modify user-library-read user-read-email user-read-private"
 
-client_data = {"client_id": "YOUR_CLIENT_ID",
-               "client_secret": "YOUR_CLIENT_SECRET"}
+client_data = {"client_id": "5c721a3a40a34fd1b4e0d45e581e6af2",
+               "client_secret": "43bd4a53899a4f63b20d9ee616776c4f"}
 
 # Spotify's login details
 creds = SpotifyClientCredentials(**client_data)
-auth = SpotifyOAuth({**client_data, "scope": scope,
+auth = SpotifyOAuth(**client_data, **{"scope": scope,
                     "redirect_uri": "http://localhost:8080"})
 
 spotify = spotipy.Spotify(auth_manager=auth)
