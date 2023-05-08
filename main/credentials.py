b2bb3cb1865b525b64321d72b925d6a296f91f95
Roxx <59770086+403CTRLer@users.noreply.github.com>
2023-05-09 00:49:24 +0530

    Update credentials.py
    Credentials changes

--- main/credentials.py
+++ main/credentials.py
@@ -4,8 +4,8 @@ from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 # Scope for required features
 scope = "ugc-image-upload user-read-recently-played user-top-read user-read-playback-position user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control streaming playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-follow-modify user-follow-read user-library-modify user-library-read user-read-email user-read-private"
 
-client_data = {"client_id": "5c721a3a40a34fd1b4e0d45e581e6af2",
-               "client_secret": "43bd4a53899a4f63b20d9ee616776c4f"}
+client_data = {"client_id": "YOUR_CLIENT_ID", # Enter your client ID from the app page on developer portal
+               "client_secret": "YOUR_CLIENT_SECRET"} # Enter your client secret from the app page on developer portal
 
 # Spotify's login details
 creds = SpotifyClientCredentials(**client_data)
